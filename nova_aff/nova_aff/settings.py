ad3d6c862a7899749c3fb0b0a47be08025dd2e53
Admin <admin@vinasupport.com>
2025-08-21 21:54:04 +0700

    3

--- nova_aff/nova_aff/settings.py
+++ nova_aff/nova_aff/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'novaaff'),
-        'USER': os.getenv('POSTGRES_USER', 'nova_user'),
+        'USER': os.getenv('POSTGRES_USER', 'novauser'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', '@12345'),
         'HOST': os.getenv('POSTGRES_HOST', '127.0.0.1'),
         'PORT': os.getenv('POSTGRES_PORT', '5432'),
